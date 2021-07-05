65d346ea7ff1d6d982cc040104556bf475b6dc1e
Raulinhotic <schluet0r4life@gmail,com>
2021-07-06 00:09:07 +0200

    added and integrated topics slice

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -3,8 +3,11 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import { useDispatch } from "react-redux";
+import { addTopic } from "../features/topics/topicsSlice";
 
 export default function NewTopicForm() {
+  const dispatch = useDispatch();
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
@@ -16,6 +19,7 @@ export default function NewTopicForm() {
     }
 
     // dispatch your add topic action here
+    dispatch(addTopic({ name: name, id: uuidv4(), icon: icon}))
     history.push(ROUTES.topicsRoute());
   };
 
--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,1 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            const { id, name, icon } = action.payload;
+            // create a new topics object based on the properties of the action.payload and add an quizIds property for all related quizzes to the topic
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon: icon,
+                quizIds: []
+            }
+        }
+    }
+})
+
+export const { addTopic } = topicsSlice.actions; 
+export default topicsSlice.reducer;
+export const selectTopics = (state) => state.topics.topics;
